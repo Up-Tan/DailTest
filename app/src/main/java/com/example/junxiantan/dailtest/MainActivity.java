5206d2403464708ffba615846774d8afdb60b6fb
Up-Tan <junxiantan@outlook.com>
2019-02-18 14:58:36 +0800

    不知道为什么无法调用到系统拨号界面

--- app/src/main/java/com/example/junxiantan/dailtest/MainActivity.java
+++ app/src/main/java/com/example/junxiantan/dailtest/MainActivity.java
@@ -18,18 +18,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        editText = (EditText) findViewById(R.id.num);
-//        Button myBtn = (Button) findViewById(R.id.mybtn);
-//
-//        myBtn.setOnClickListener(new OnClickListener() {
-//            public void onClick(View v) {
-//                Uri uri=Uri.parse("tel:"+ editText.getText().toString());
-//                //将字符串转换成Uri对象
-//                Intent intent = new Intent(Intent.ACTION_CALL, uri);
-//                //第一个参数表示操作的动作，系统根据这个会调用拨号功能
-//                //第二个参数用于指定操作的数据，即向拨打哪个号码
-//                MainActivity.this.startActivity(intent);
-//            }
-//        });
+        editText = (EditText) findViewById(R.id.num);
+        Button myBtn = (Button) findViewById(R.id.mybtn);
+
+        myBtn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                //将字符串转换成Uri对象
+                Uri uri=Uri.parse("tel:"+ editText.getText().toString());
+
+
+                //第一个参数表示操作的动作，系统根据这个会调用拨号功能
+                //第二个参数用于指定操作的数据，即向拨打哪个号码
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
